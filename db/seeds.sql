e8bce7018066339ffcf2b46a897e1980fd2f5f57
Jenny Oum <jennyoum@hotmail.com>
2019-08-16 00:04:31 -0400

    added folders and template

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Wagyu');
+INSERT INTO burgers (burger_name) VALUES ('BaconCheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('DoubleCheeseburger', true);
